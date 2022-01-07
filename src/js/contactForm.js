941d183d36b97fe251205c824916bf6a73b4173d
Jonathan Rousseau <79579994+jonathan-rousseau@users.noreply.github.com>
2022-01-07 13:59:59 +0100

    add sweetAlert

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -3,6 +3,7 @@ let name = document.getElementById('name');
 let email = document.getElementById('email');
 let subject = document.getElementById('subject');
 let message = document.getElementById('message')
+import Swal from 'sweetalert2';
 
 contactForm.addEventListener('submit',(e)=>{
     e.preventDefault()
@@ -18,6 +19,13 @@ contactForm.addEventListener('submit',(e)=>{
     xhr.onload = function(){
         console.log(xhr.responseText);
         if(xhr.responseText == 'success'){
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Votre email a bien été envoyé',
+                showConfirmButton: false,
+                timer: 3000,
+              })
             alert('Votre email a bien été envoyé');
             name.value = '';
             email.value = '';
